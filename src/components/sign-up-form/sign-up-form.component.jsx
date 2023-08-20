6db3bf5a52d91300f958430f199cb211a624e545
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2023-08-20 20:05:53 +0600

    custom button component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -5,6 +5,9 @@ import {
 } from "../../utils/firebase/firebase.utils";
 import FormInput from "../form-input/form-input.component";
 
+import './sign-up-form.styles.scss';
+import Button from "../button/button.component";
+
 const defaultFormFields = {
   displayName: "",
   email: "",
@@ -52,8 +55,9 @@ const SignUpForm = () => {
   };
 
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className="sign-up-container">
+      <h2>Don't have an account</h2>
+      <span>Sign up with your email and password</span>
 
       <form onSubmit={handleSubmit}>
         <FormInput
@@ -91,7 +95,7 @@ const SignUpForm = () => {
           name="confirmPassword"
           value={confirmPassword}
         />
-        <button>Sign Up</button>
+        <Button>Sign Up</Button>
       </form>
     </div>
   );
